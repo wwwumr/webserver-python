0cfbdb39dcfbe7a2011d36d8b1a1364683a3d757
wwwumr <1127858301@qq.com>
2019-12-07 21:49:56 +0800

    解决docker外部访问问题

--- web-server/try.py
+++ web-server/try.py
@@ -11,4 +11,4 @@ def hello_world():
 if __name__ == '__main__':
     # app.run(host, port, debug, options)
     # 默认值：host=127.0.0.1, port=5000, debug=false
-    app.run()
+    app.run(host=0.0.0.0, port=5000, debug=False)
